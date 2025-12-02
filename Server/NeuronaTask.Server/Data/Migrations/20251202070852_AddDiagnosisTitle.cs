be122b97828fa55eaabfc5fff09f14ac71e6b846
Patrik Schweika <patrik.schweika@gmail.com>
2025-12-02 17:35:00 +1000

    Backend: add diagnosis title migration

---
+++ Server/NeuronaTask.Server/Data/Migrations/20251202070852_AddDiagnosisTitle.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NeuronaTask.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiagnosisTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Diagnoses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Diagnoses");
+        }
+    }
+}
